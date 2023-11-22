e71326c5f57ffb75752788cb791992ceeb97e3f1
dpcks0509 <dpcks0509@gmail.com>
2023-11-22 14:39:59 +0900

    test: 게임 재시작 여부 입력 예외처리 테스트 추가
    게임 재시작 여부 입력에 대한 예외처리 테스트를 추가하였다.
    1. 숫자가 아닌 값을 입력하거나 공백이 존재한다.
    2. 1과 2 중 하나의 수가 아니다.

--- src/test/kotlin/baseball/BaseballTest.kt
+++ src/test/kotlin/baseball/BaseballTest.kt
@@ -1,6 +1,7 @@
 package baseball
 
 import baseball.util.Validator.validateAnswerNumbers
+import baseball.util.Validator.validateGameDecision
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
@@ -9,31 +10,49 @@ import org.junit.jupiter.params.provider.ValueSource
 class BaseballTest {
     @ParameterizedTest
     @ValueSource(strings = ["123", "456", "789"])
-    fun `올바른 정답 입력`(input: String) {
+    fun `올바른 게임 플레이어 정답 입력`(input: String) {
         assertDoesNotThrow { validateAnswerNumbers(input) }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["one23", "123 ", " 123"])
-    fun `정답 입력 예외처리 (숫자가 아닌 값을 입력하거나 공백이 존재한다)`(input: String) {
+    fun `게임 플레이어 정답 입력 예외처리 (숫자가 아닌 값을 입력하거나 공백이 존재한다)`(input: String) {
         assertThrows<IllegalArgumentException> { validateAnswerNumbers(input) }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["1", "12 ", " 1234"])
-    fun `정답 입력 예외처리 (숫자의 길이가 3이 아니다)`(input: String) {
+    fun `게임 플레이어 정답 입력 예외처리 (숫자의 길이가 3이 아니다)`(input: String) {
         assertThrows<IllegalArgumentException> { validateAnswerNumbers(input) }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["012", "102 ", " 120"])
-    fun `정답 입력 예외처리 (숫자 0이 존재한다)`(input: String) {
+    fun `게임 플레이어 정답 입력 예외처리 (숫자 0이 존재한다)`(input: String) {
         assertThrows<IllegalArgumentException> { validateAnswerNumbers(input) }
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["112", "122 ", " 212"])
-    fun `정답 입력 예외처리 (중복되는 숫자가 존재한다)`(input: String) {
+    fun `게임 플레이어 정답 입력 예외처리 (중복되는 숫자가 존재한다)`(input: String) {
+        assertThrows<IllegalArgumentException> { validateAnswerNumbers(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "2"])
+    fun `올바른 게임 재시작 여부 입력`(input: String) {
+        assertDoesNotThrow { validateGameDecision(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["one", "two", " 1", " 2 "])
+    fun `게임 재시작 여부 입력 예외처리 (숫자가 아닌 값을 입력하거나 공백이 존재한다)`(input: String) {
+        assertThrows<IllegalArgumentException> { validateAnswerNumbers(input) }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["-1", "0 ", " 3"])
+    fun `게임 재시작 여부 입력 예외처리 (1과 2 중 하나의 수가 아니다)`(input: String) {
         assertThrows<IllegalArgumentException> { validateAnswerNumbers(input) }
     }
 }
